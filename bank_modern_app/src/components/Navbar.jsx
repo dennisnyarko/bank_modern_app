c44f969c5c9722d88aa53a2bcef2c1dd6b062af0
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-02 16:27:10 +0000

    update

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -5,6 +5,8 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"/>
@@ -13,12 +15,17 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li
           key={nav.id}
-          className={'font-poppins font-normal cursor-pointer text-[16px] text-white'}
+          className={'font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-white'}
           >
             <a href={'#${nav.id'}>{nav.title}</a>
           </li>
         ))}
       </ul>
+
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? close : menu}/>
+
+          </div>
     </nav>
   )
 }
