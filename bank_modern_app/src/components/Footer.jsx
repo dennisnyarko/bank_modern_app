3e7949688e1688e93022f801dbdd951a87a3a8ee
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-05 14:05:46 +0000

    add bank logo to footer

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import styles from "../style"
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
+const Footer = () => (
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexSart} md:flex-row flex-col mb-8 w-full`}>
+      <div>
+        <img src={logo} alt="hoobank" className="w-[266px] h-[72px] object-contain"/>
+      </div>
+
+      </div>
+    </section>
   )
-}
 
 export default Footer
