1cd12a61813325457919fa01652995e4e815f141
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-01-05 16:02:24 +0000

    corrected spelling

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-      <div className={`${styles.flexSart} md:flex-row flex-col mb-8 w-full`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className="flex-1 flex flex-col justify-start mr-10">
         <img src={logo} alt="hoobank" className="w-[266px] h-[72px] object-contain"/>
         <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
@@ -36,7 +36,7 @@ const Footer = () => (
 
       <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
         <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        2021 HooBank. All Rights Reserved.
+        Copyright © 2021 HooBank. All Rights Reserved.
         </p>
 
         <div className="flex flex-row md:mt-0 mt-6">
